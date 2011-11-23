f4499cecf91f2f5b4d03a505570196c41f82d87a
radunh <radunh@comcast.net>
2011-11-23 11:39:39 -0500

    testing a change

--- api/src/test/java/com/springsource/html5expense/impl/JpaExpenseReportingServiceTest.java
+++ api/src/test/java/com/springsource/html5expense/impl/JpaExpenseReportingServiceTest.java
@@ -31,7 +31,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;  
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.inject.Inject;
